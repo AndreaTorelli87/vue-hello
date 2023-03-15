ff05695e9012744422073752e7c7e0905502aae2
Andrea Torelli <gioran326@protonmail.com>
2023-03-15 13:14:11 +0100

    aggiustamenti

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ createApp({
     return {
       message: "Clicca sull'immagine per modificarla",
       immagine: "https://picsum.photos/600/",
-      classiH1: "m-5 fw-bold",
-      classiDivApp: "container my-5 text-center"
+      classiH1: "m-5 p-5 fw-bold",
+      classiDivApp: "py-5 text-center"
     }
   },
   methods: {
     reload() {location.reload();},
-    generaColoreRandom() {
+    generaColoreRandom(proprieta) {
       let characters = "0123456789abcdef"
-      let str = "color: #"
+      let str = `${proprieta}: #`
       for(let i = 0; i < 6; i++){
           str += characters[Math.floor(Math.random() * 16)]
       }
